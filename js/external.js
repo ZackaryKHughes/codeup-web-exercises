301eeed25d6a8b37aa45845383227badcd94b901
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-04-14 09:46:56 -0500

    Added two new HTML files, inline_js and external_js. Created js folder with a file inside named external.js.

--- js/external.js
+++ js/external.js
@@ -1,1 +1,16 @@
+"use strict";
+
+
 console.log("Whats up Dragon?");
+
+
+//alert("Hey Dragon.");
+
+// the following line will show the OK/CANCEL confirm dialog
+//var confirmed = confirm('Are you sure you want to do XYZ?');
+//console.log(confirmed); // will be either true or false
+//
+//console.log("Hello Again");
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
