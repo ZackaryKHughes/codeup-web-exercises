cc3afe2fe5e7e8c2ef2720e6609e4d464e6b743b
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-04-14 10:40:16 -0500

    Added fuction that promt alert questions to find out total rental price for 3 movies

--- js/external.js
+++ js/external.js
@@ -12,8 +12,30 @@ alert("Welcome to my Website!");
 //
 //console.log("Hello Again");
 
+// Question 1
 var userInput = prompt('What is your favorite color?');
 console.log('The user entered: ' + userInput);
 
 
 alert(userInput + " is my favorite color as well!!!");
+
+
+alert("I hear you got some movies for you kids to watch and wanna know how much the rental will be.")
+
+alert("I think I can help you with that.")
+
+//Question 2
+function rentalPrice() {
+    let lm = prompt("How many days for Little Mermaid?");
+    let bb = prompt("How man days for Brother Bear?");
+    let hercules = prompt("How many days for Hercules?");
+    let day = 3
+    let price = (Number(lm) + Number(bb) + Number(hercules)) * day;
+    return price;
+
+}
+
+alert("Your rental total is " + "$" + rentalPrice() +".00");
+
+//Quetion 3
+
