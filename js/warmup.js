e6033fb83df8e740b3a5b2cd316534f8d450b528
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-05-01 09:37:35 -0500

    Finished 05012023 morning warmup

--- js/warmup.js
+++ js/warmup.js
@@ -7,3 +7,30 @@ function seven(){
 }
 
 console.log(seven());
+
+console.log("_______________________________________")
+
+function findFactors(x){
+    if (typeof x === "number" && !isNaN(x)){
+        let result = [];
+       for (let i = 1; i <= x; i++){
+           if(x % i === 0){
+               result.push(i)
+           }
+       }
+        return result;
+    } else {
+        return false
+    }
+}
+
+console.log(findFactors(16));;
+console.log(findFactors(6));;
+console.log(findFactors(0));;
+console.log(findFactors(true));; // returns false;
+console.log(findFactors("13"));; // returns false;
+console.log(findFactors([54, 72, 144]));; // returns false;
+console.log(findFactors({value: 64}));; // returns false;
+console.log(findFactors());; // returns false;
+
+console.log("____________________________________")
