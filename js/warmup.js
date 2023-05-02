87019031a1bb092c874f523c892d17d45987d987
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-05-02 09:41:41 -0500

    Finished style css exercises to welcome.html

--- js/warmup.js
+++ js/warmup.js
@@ -34,3 +34,27 @@ console.log(findFactors({value: 64}));; // returns false;
 console.log(findFactors());; // returns false;
 
 console.log("____________________________________")
+
+
+function countEs(x) {
+    if (typeof x === "string") {
+        let count = 0;
+        let E = x.toUpperCase();
+        for (let i = 0; i <+ x.length; i++) {
+            if(E.charAt(i) === "E") {
+                count ++;
+            }
+            return count
+        }
+    } else {
+        return false
+    }
+
+}
+
+console.log(countEs("Ease")); // returns 2;
+console.log(countEs("teleconference")); // returns 5;
+console.log(countEs("TOM")); // returns 0;
+console.log(countEs(true)); // returns false;
+console.log(countEs(['e', 'E'])); // returns false;
+console.log(countEs()); // returns false;
