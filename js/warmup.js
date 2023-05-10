2f96dd39a618def2151eceffd49c62775f107a41
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-05-10 09:17:35 -0500

    Finished JS warmup 5/10/2023.

--- js/warmup.js
+++ js/warmup.js
@@ -108,5 +108,19 @@ do {
 
 console.log("-----------------------------------------")
 
+function addEmUp(x) {
+    let total = 0;
+    for (let i = 0; i < x.length; i++) {
+        total += x[i];
+    } return total;
+}
+
+console.log(addEmUp([2,6,19])); // returns 27
+console.log(addEmUp([-99, 180, -5])); // returns 76
+console.log(addEmUp([44,10,7])); // returns 61
+console.log(addEmUp([-100])); // returns -100
+console.log(addEmUp([1,2,3,4,5,6,7,8,9,10])); // returns 55
+console.log(addEmUp([-13, -92, -3500])) // returns -3605
+
 
 
