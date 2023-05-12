9d67444d5dce11518bc819f287821bd0d3ed1b5f
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-05-12 09:26:45 -0500

    finished 5/12/2023 warmup

--- js/warmup.js
+++ js/warmup.js
@@ -141,4 +141,55 @@ console.log(explainString("cheese")) // returns {string: "cheese", numberOfEs: 3
 console.log(explainString("dog")) // returns {string: "dog", numberOfEs: 0, isEvenLength: false}
 
 
+console.log("-----------------------------------------")
+
+
+
+// Write a function that takes in an array of objects and returns an array containing all of the names from the original array.
+
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "September 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
+
+function extractNames(x) {
+    let bucket = [];
+    x.forEach(function(y){
+        bucket.push(y.name);
+    });
+return bucket;
+}
+
+console.log(extractNames(hamsters)); // returns ["Hamtaro", "Bijou", "Oxnard", "Boss", "Snoozer"];
+
+
 
