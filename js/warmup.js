a7e30ba4fbfaf7a18c22dcf28006417c41270c2d
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-05-18 09:26:49 -0500

    Finished 05/18/2023

--- js/warmup.js
+++ js/warmup.js
@@ -246,3 +246,22 @@ function mostColorful(x) {
 }
 
 console.log(mostColorful(hamsters)); // returns {name: "Snoozer", heightInMM: 85, fur: ['brown', 'white', "pink"], gender: "male", dateOfBirth: "January 14"};
+
+
+console.log("-----------------------------------------")
+
+
+// Write a JavaScript function that takes in a number and returns an object with the following fields: number, which will contain the original number; evenOrOdd, which will contain a string ("even" or "odd") as to whether the value is even or odd; factors, an array of numbers that are evenly divisible within the number passed; and numberOfDigits, a number counting the number of digits with the number passed.
+
+function describeNumber(num) {
+    return {
+        number: num,
+        evenOrOdd:  (num % 2 === 0) ? "even" : "odd",
+        factors: findFactors(num),
+        numberOfDigits: (num.toString.length)
+    }
+}
+
+
+console.log(describeNumber(19)); // returns {number: 19, evenOrOdd: "odd", factors: [1,19], numberOfDigits: 2};
+console.log(describeNumber(2)); // returns {number: 2, evenOrOdd: "even", factors: [1,2], numberOfDigits: 1};
