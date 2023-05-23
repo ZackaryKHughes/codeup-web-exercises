3ca652f1462398a9defa896d0b0f5f8bfda07445
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-05-23 11:30:17 -0500

    Finished jQuery exercise

---
+++ js/jquery.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// var contents = $('#codeup').html();
+// alert(contents);
+//
+$(function () {
+  let title = $("h1").html();
+  alert(title);
+});
+//
+// $(function () {
+//   $(".codeup").css('border', '1px solid red')
+// })
+
+$(function () {
+  $("li").css("font-size", "20px")
+})
+
+$(function () {
+  $("h1, p, li").css("background-color", "yellow")
+})
+
+
