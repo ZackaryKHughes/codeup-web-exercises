430f238a4b265a600d059711263447fa5aed0bfd
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-05-24 10:07:18 -0500

    Finsished final product of konami exercise

--- js/jquery.js
+++ js/jquery.js
@@ -1,23 +1,42 @@
 "use strict";
 
-// var contents = $('#codeup').html();
-// alert(contents);
-//
-$(function () {
-  let title = $("h1").html();
-  alert(title);
+$(document).keyup(function(event){
+    console.log(event.keyCode);
 });
-//
-// $(function () {
-//   $(".codeup").css('border', '1px solid red')
-// })
 
-$(function () {
-  $("li").css("font-size", "20px")
-})
 
-$(function () {
-  $("h1, p, li").css("background-color", "yellow")
-})
+let pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+let current = 0;
+let lives = $(".lives");
+let mario = $(".mario");
+let mushroom = $(".mushroom");
+
+let keyHandler = function (event) {
+
+    // If the key isn't in the pattern, or isn't the current key in the pattern, reset
+    if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+        current = 0;
+        return;
+    }
+
+    // Update how much of the pattern is complete
+    current++;
+
+    // If complete, alert and reset
+    if (pattern.length === current) {
+        current = 0;
+        window.alert('You found the secret code.....');
+        window.alert('What is happening to me.....')
+        mushroom.animate({
+            "margin-right" : "+=125em",
+        }, 1500);
+        lives.html("30")
+        mario.animate({fontSize: '2em'}, "slow");
+    }
+};
+
+// Listen for keydown events
+document.addEventListener('keyup', keyHandler, false);
+
 
 
