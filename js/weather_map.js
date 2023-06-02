afa69f39ad528cd05d35255560b516bb8ec1b3e6
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-06-02 13:49:59 -0500

    Finished Search bar JS function to work with city name. Also made a weatherData function that can be called on in other functions so code doesn't get repeatable.

--- js/weather_map.js
+++ js/weather_map.js
@@ -17,24 +17,9 @@
   let latLong =[-97.238075, 33.261505];
   $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latLong[0]}&lon=${latLong[1]}&units=imperial&appid=${weatherMapKey}`).done(function(data){
     console.log(data)
-    let todayDate = new Date(data.list[0].dt * 1000);
-    let month;
-    let day;
-
-
-    let weatherHTML = `<section><h4>${day}, ${month}, ${todayDate.getDate()}</h4><p>${data.list[0].main.temp_max} / ${data.list[0].main.temp_min}</p><p>${data.list[0].wind.speed + 'mph'}</p><p>${data.list[0].weather[0].main}</p></section>`;
-    $('#weather-info3').html(weatherHTML);
   })
 
 
-  let updatedCoordinates = [];
-  const marker = new mapboxgl.Marker({
-    draggable: true,
-  })
-      .setLngLat(latLong)
-      .addTo(map);
-
-  marker.on("dragend", onDragEnd);
 
   $(window).on("load", function () {
     $.get(
@@ -45,32 +30,19 @@
       $("#card-container").empty();
       $(".header")
           .empty()
-          .append(`<h1>${data.city.name}, ${data.city.country}</h1>`);
-      data.list.forEach(function (item, index) {
-        let milliseconds = (item.dt * 1000)
-        if (index % 8 === 0) {
-          $("#card-container").append(`<div class="card col">
-  <div class="card-header">
-    <h6>${new Date(milliseconds).toDateString()}</h6>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Temp: ${item.main.temp.toFixed(0)}&#8457</li>
-    <li class="list-group-item">Feels Like: ${item.main.feels_like.toFixed(
-              0
-          )}&#8457</li>
-        <li class="list-group-item">Wind: ${item.wind.speed.toFixed(0)}mph</li>
-    <li class="list-group-item">Chance of Rain: ${(item.pop * 100).toFixed(
-              0
-          )} % </li>
-  </ul>
-</div>`);
-        } else {
-        }
-      });
+          .append(`<h1>${data.city.name} ${data.city.country}</h1>`);
+      weatherData(data)
     });
   });
 
+  let updatedCoordinates = [];
+  const marker = new mapboxgl.Marker({
+    draggable: true,
+  })
+      .setLngLat(latLong)
+      .addTo(map);
 
+  marker.on("dragend", onDragEnd);
 
   function onDragEnd() {
     updatedCoordinates = [];
@@ -85,37 +57,56 @@
 
       $("#card-container").empty();
       $(".header").empty();
-      $(".header").append(`<h1 class="text-info">${data.city.name}, ${data.city.country}</h1>`);
-      data.list.forEach(function (item, index) {
-        let milliseconds = (item.dt * 1000)
-        if (index % 8 === 0) {
-          $("#card-container")
-              .append(`<div class="card col">
+      $(".header").append(`<h1 class="text-info">${data.city.name} ${data.city.country}</h1>`);
+      weatherData(data)
+    });
+  }
+
+  let searchLocation = document.querySelector("#user-search");
+
+  $("#user-search-button").on("click", function (event) {
+    event.preventDefault();
+    marker.remove();
+    let userLocation = geocode(`${searchLocation.value}`, weatherMapBox);
+    userLocation.then(function (data) {
+      marker.setLngLat(data).addTo(map);
+      map.flyTo({
+        center: data,
+        essential: true,
+      });
+      $.get(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${data[1]}&lon=${data[0]}&units=imperial&appid=${weatherMapKey}`
+      ).done(function (data) {
+        $("#card-container").empty();
+        $(".header").empty();
+        $(".header").append(`<h1 class="text-info">${data.city.name} ${data.city.country}</h1>`);
+        weatherData(data)
+      });
+    });
+  });
+
+
+  function weatherData(data) {
+    data.list.forEach(function (item, index) {
+      let milliseconds = (item.dt * 1000)
+      if (index % 8 === 0) {
+        $("#card-container").append(`<div class="card col">
   <div class="card-header">
     <h6>${new Date(milliseconds).toDateString()}</h6>
   </div>
   <ul class="list-group list-group-flush">
-   <li class="list-group-item">Temp: ${item.main.temp.toFixed(0)}&#8457</li>
+    <li class="list-group-item">Temp: ${item.main.temp.toFixed(0)}&#8457</li>
     <li class="list-group-item">Feels Like: ${item.main.feels_like.toFixed(
-                  0
-              )}&#8457</li>
+            0
+        )}&#8457</li>
+        <li class="list-group-item">Wind: ${item.wind.speed.toFixed(0)}mph</li>
     <li class="list-group-item">Chance of Rain: ${(item.pop * 100).toFixed(
-                  0
-              )} % </li>
-    <li class="list-group-item">Wind: ${item.wind.speed.toFixed(0)}mph</li>
+            0
+        )} % </li>
   </ul>
 </div>`);
-        }
-      });
+      }
     });
   }
-
-
-
-
-
-
-
-
 
 })();
